f729d7979c4cfd937f3f7915d2c038ffeaa4a7b7
Patrick-Cuppi <cuppi.patrick@gmail.com>
2025-04-07 16:27:07 -0300

    feat: get data of the user

--- src/@controller/usersController.ts
+++ src/@controller/usersController.ts
@@ -3,6 +3,19 @@ import type { AuthenticatedRequest } from '../@middlewares/auth'
 import { userService } from '../@services/userService'
 
 export const usersController = {
+  show: async (req: AuthenticatedRequest, res: Response) => {
+    try {
+      // biome-ignore lint/style/noNonNullAssertion: <explanation>
+      const currentUser = req.user!
+
+      return res.json(currentUser)
+    } catch (error) {
+      if (error instanceof Error) {
+        return res.status(400).json({ message: error.message })
+      }
+    }
+  },
+
   watching: async (req: AuthenticatedRequest, res: Response) => {
     // biome-ignore lint/style/noNonNullAssertion: <explanation>
     const { id } = req.user!
